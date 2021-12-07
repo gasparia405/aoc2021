af0f2ddc60599f00090a9034dcda66f954c67557
Andrew Gaspari <andrew.gaspari@stjude.org>
2021-12-07 10:12:36 -0600

    Add Task 2 solution for December 7

--- dec_7/dec_7.py
+++ dec_7/dec_7.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 
 # Import data
-with open('/home/alennon/aoc2021/dec_7/dec7_input.txt') as f:
+with open('/home/agaspari/aoc2021/dec_7/dec7_input.txt') as f:
     crab_list = sorted([int(x) for x in f.read().split(',')])
 
 # Task 1
 # Find the closest number to all numbers in the list
-low = 0
-high = len(crab_list) - 1
 lowest_sum = sum([abs(crab_list[-1] - x) for x in crab_list])
-proxy_crab = crab_list
 for value in range(0, crab_list[-1]):
     new_sum = sum([abs(value - x) for x in crab_list])
     if new_sum > lowest_sum:
@@ -19,3 +16,17 @@ for value in range(0, crab_list[-1]):
         continue
 
 print(lowest_sum)
+
+# Task 2
+lowest_sum = sum([abs(crab_list[-1] - x) * abs(crab_list[-1] - x) for x in crab_list])
+for value in range(0, crab_list[-1]):
+    dist_list = [abs(value - x) * (abs(value - x) + 1) // 2 for x in crab_list]
+    new_sum = sum(dist_list)
+    if new_sum > lowest_sum:
+        continue
+    else:
+        lowest_sum = new_sum
+
+print(lowest_sum)
+# Get the minimum value in a list of sums of the list of distances from values for each crab position
+print(min([sum([abs(value - x) * (abs(value - x) + 1) // 2 for x in crab_list]) for value in range(0, crab_list[-1])]))
